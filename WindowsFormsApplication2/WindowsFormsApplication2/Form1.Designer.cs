90082d727dbb94c32e246fe5b0b6b1d9102b84d9
Pierre Goyette <pierre.goyette@outlook.com>
2013-04-17 22:21:17 -0400

    repos

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(85, 103);
+            this.button1.Location = new System.Drawing.Point(195, 220);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
